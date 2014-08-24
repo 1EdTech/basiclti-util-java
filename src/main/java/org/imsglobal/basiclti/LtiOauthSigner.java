b51e0820f7b19e05ba6d3e275d80591cf8259bd8
Paul Gray <pfbgray@gmail.com>
2014-08-23 20:54:31 -0400

    Adding service class to sign LTI requests

---
+++ src/main/java/org/imsglobal/basiclti/LtiOauthSigner.java
@@ -0,0 +1,7 @@
+package org.imsglobal.basiclti;
+
+/**
+ * Created by pgray on 8/23/14.
+ */
+public class LtiOauthSigner {
+}
---
+++ src/main/java/org/imsglobal/basiclti/LtiSigner.java
@@ -0,0 +1,12 @@
+package org.imsglobal.basiclti;
+
+import org.apache.http.HttpRequest;
+
+/**
+ * Created by pgray on 8/23/14.
+ */
+public interface LtiSigner {
+
+    public HttpRequest sign(HttpRequest request);
+
+}
